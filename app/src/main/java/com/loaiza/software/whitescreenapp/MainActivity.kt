69e6f36db06d9ba0c2b6b710dbf7703e6fb8676c
Julio Segura <julio.segura@encora.com>
2022-12-15 10:29:49 -0600

    In App Updates

--- app/src/main/java/com/loaiza/software/whitescreenapp/MainActivity.kt
+++ app/src/main/java/com/loaiza/software/whitescreenapp/MainActivity.kt
@@ -1,15 +1,36 @@
 package com.loaiza.software.whitescreenapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var inAppUpdate: InAppUpdate
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        inAppUpdate = InAppUpdate(this)
 
         //Remove de shadow from action bar:
         supportActionBar?.elevation = 0F
 
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        inAppUpdate.onActivityResult(requestCode,resultCode, data)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        inAppUpdate.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        inAppUpdate.onDestroy()
+    }
+
 }
